80131b58591ee478054f5426337fb70da224b3cb
sabski03 <saba27ge@gmail.com>
2024-04-21 16:40:37 +0400

    creating posts

--- includes/navigation.php
+++ includes/navigation.php
@@ -23,7 +23,7 @@
                     while($row = mysqli_fetch_assoc($select_all_categories_query)){
                         $cat_title = $row['cat_title'];
 
-                        echo "<li><a href='#'> $cat_title</a></li>";
+                        echo "<li><a href='#'>$cat_title</a></li>";
                     }
 
                 ?>
